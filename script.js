0cfd20dbe721d333b7ec8b5d4e864b925cf33b3c
Rahvv <leemienti@gmail.com>
2022-01-26 18:43:24 +0800

    Create playRound function.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3) +1;
+    switch (randomNumber) {
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors";
+            break;
+    }
+}
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else {return "It's a tie!";
+    }
+}
