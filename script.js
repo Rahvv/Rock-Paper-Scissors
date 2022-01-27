f5fea67fa64a569aef327d52602c48900c967497
Rahvv <leemienti@gmail.com>
2022-01-27 09:56:05 +0800

    Finalized game function.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let score;
+
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
     switch (randomNumber) {
@@ -13,19 +15,44 @@ function computerPlay() {
     }
 }
 
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        score++;
         return "You Win! Rock beats Scissors";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
         return "You Lose! Paper beats Rock";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        score++;
         return "You Win! Paper beats Rock";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
         return "You Lose! Scissors beats Paper";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        score++;
         return "You Win! Scissors beats Paper";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
         return "You Lose! Rock beats Scissors";
     } else {return "It's a tie!";
+    }
+}
+
+function game() {
+    score = 0
+    let playerSelection = window.prompt("What do you choose: Rock, Paper or Scissors?")
+    console.log(playRound(playerSelection, computerPlay()));
+    playerSelection = window.prompt("What do you choose: Rock, Paper or Scissors?")
+    console.log(playRound(playerSelection, computerPlay()));
+    playerSelection = window.prompt("What do you choose: Rock, Paper or Scissors?")
+    console.log(playRound(playerSelection, computerPlay()));
+    playerSelection = window.prompt("What do you choose: Rock, Paper or Scissors?")
+    console.log(playRound(playerSelection, computerPlay()));
+    playerSelection = window.prompt("What do you choose: Rock, Paper or Scissors?")
+    console.log(playRound(playerSelection, computerPlay()));
+    console.log("You got a score of " + score + " out of 5!");
+    if (score >= 3) {
+        console.log('You win!');
+    } else {
+        console.log('You lose!');
     }
 }
